ce42fe0353387c8fc6848716a9a2522c4318d136
Gxu6451 <98739284+Gxu6451@users.noreply.github.com>
2022-06-16 17:50:58 +0800

    Update 12-2.py

--- 12/12-2.py
+++ 12/12-2.py
@@ -2,18 +2,6 @@ import re
 read_file=open("in.txt",'r',encoding='UTF-8')
 a=read_file.read()
 b=re.sub('\d',' ',a)
-c=re.sub('[+=-]',' ',b)
-d=b.split()
-#print(a)
-#print(b)
-print(len(d),len(a)-1)
-'''read_file=open("in.txt",'r',encoding='UTF-8')
-a=read_file.read()
-import re
-read_file=open("in.txt",'r',encoding='UTF-8')
-a=read_file.read()
-print(a)
-#print(len(a.split()),len(a)-1)
-for i in range(0,9):
-    b=str.replace('i',' ')
-    print(b)'''
+str=re.sub('[+=-]',' ',b)
+c=str.split()
+print(len(c),len(a)-1)
